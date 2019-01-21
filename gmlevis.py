4abc0a56553960e76df58d33b76af7a5d2827a0f
anilsomani <theanilsomani@gmail.com>
2019-01-20 16:55:22 -0800

    Fixed indentation

--- gmlevis.py
+++ gmlevis.py
@@ -1,29 +1,29 @@
 import io
-  2 import sys
-  3 from google.cloud import vision
-  4 from google.cloud.vision import types
-  5
-  6 vision_client = vision.ImageAnnotatorClient();
-  7
-  8 if len(sys.argv) < 2:
-  9     print("Filename missing")
- 10     sys.exit();
- 11
- 12 file_name = sys.argv[1];
- 13 print("Scanning file " + file_name)
- 14
- 15 with io.open(file_name, 'rb') as image_file:
- 16     content = image_file.read();
- 17     image = types.Image(content=content)
- 18
- 19 response = vision_client.label_detection(image=image)
- 20 labels = response.label_annotations
- 21
- 22 for label in labels:
- 23     print(label.description)
- 24
- 25 response = vision_client.document_text_detection(image=image)
- 26 texts = response.text_annotations
- 27
- 28 for text in texts:
- 29     print(text.description)
+import sys
+from google.cloud import vision
+from google.cloud.vision import types
+
+vision_client = vision.ImageAnnotatorClient();
+
+if len(sys.argv) < 2:
+  print("Filename missing")
+  sys.exit();
+
+file_name = sys.argv[1];
+print("Scanning file " + file_name)
+
+with io.open(file_name, 'rb') as image_file:
+  content = image_file.read();
+  image = types.Image(content=content)
+
+response = vision_client.label_detection(image=image)
+labels = response.label_annotations
+
+for label in labels:
+  print(label.description)
+
+response = vision_client.document_text_detection(image=image)
+texts = response.text_annotations
+
+for text in texts:
+  print(text.description)
