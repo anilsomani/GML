05c1c1657f1483f10e71e38f2f5779bb8fb028e5
anilsomani <theanilsomani@gmail.com>
2019-01-20 16:48:38 -0800

    Modified to conform to updated Google ML Vision cloud client spec

--- gmlevis.py
+++ gmlevis.py
@@ -1,23 +1,29 @@
 import io
-import sys
-from google.cloud import vision
-
-vision_client = vision.Client();
-
-if len(sys.argv) < 2: 
-    print("Filename missing") 
-    sys.exit();
-
-file_name = sys.argv[1];
-print("Scanning file " + file_name)
-
-with io.open(file_name, 'rb') as image_file:
-    content = image_file.read();
-    image = vision_client.image(content=content)
-
-print(image.detect_full_text().text)
-
-labels = image.detect_labels()
-
-for label in labels:
-    print(label.description) 
+  2 import sys
+  3 from google.cloud import vision
+  4 from google.cloud.vision import types
+  5
+  6 vision_client = vision.ImageAnnotatorClient();
+  7
+  8 if len(sys.argv) < 2:
+  9     print("Filename missing")
+ 10     sys.exit();
+ 11
+ 12 file_name = sys.argv[1];
+ 13 print("Scanning file " + file_name)
+ 14
+ 15 with io.open(file_name, 'rb') as image_file:
+ 16     content = image_file.read();
+ 17     image = types.Image(content=content)
+ 18
+ 19 response = vision_client.label_detection(image=image)
+ 20 labels = response.label_annotations
+ 21
+ 22 for label in labels:
+ 23     print(label.description)
+ 24
+ 25 response = vision_client.document_text_detection(image=image)
+ 26 texts = response.text_annotations
+ 27
+ 28 for text in texts:
+ 29     print(text.description)
